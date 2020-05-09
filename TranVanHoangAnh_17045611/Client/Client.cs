1473cbc35f047dd369c8cbec87adbeb2b8442288
hoanganhtrans2 <46130871+hoanganhtrans2@users.noreply.github.com>
2020-05-09 14:35:43 +0700

    Nộp bài Client Server

---
+++ TranVanHoangAnh_17045611/Client/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 13000);
+            string message = "i am Client";
+            // chuyển text sang mảng byte
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            NetworkStream stream = client.GetStream();
+            // gửi mess tới server
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine("Sent: {0}", message);
+            data = new Byte[256];
+            String responseData = String.Empty;
+            // dọc dữ liệu phản hồi từ sơ vờ
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine("Received: {0}", responseData);
+            stream.Close();
+            client.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TranVanHoangAnh_17045611/Server/Server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Int32 port = 13000;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(localAddr, port);
+            server.Start();
+            Byte[] bytes = new Byte[256];
+            String data = null;
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");             
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+                data = null;
+                NetworkStream stream = client.GetStream();
+                int i;
+                // Lặp lại để nhận tất cả dữ liệu được gửi bởi khách hàng.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // chuyển byte sang ASSCII
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    // dữ liệ client gửi đến
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    // gửi lại phản hồi
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                }
+
+                client.Close();
+                Console.ReadLine();
+            }
+        }
+    }
+}
