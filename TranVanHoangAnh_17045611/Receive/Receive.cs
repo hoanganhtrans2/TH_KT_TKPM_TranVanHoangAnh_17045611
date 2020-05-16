4f169e98ebe200586d2e77440a6f6274974bef20
hoanganhtrans2 <46130871+hoanganhtrans2@users.noreply.github.com>
2020-05-16 15:17:21 +0700

    Nộp bài tuần 2 Send and Receive Message ActiveMQ

---
+++ TranVanHoangAnh_17045611/Receive/Receive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ.Commands;
+
+namespace Receive
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616");
+            IConnection con = factory.CreateConnection("admin", "admin");
+            con.Start();
+            ISession session = con.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            ActiveMQQueue destination = new ActiveMQQueue("HoangAnh");
+            IMessageConsumer consumer = session.CreateConsumer(destination);
+            consumer.Listener += Consumer_Listener;
+            Console.ReadLine();
+        }
+        private static void Consumer_Listener(IMessage message)
+        {
+            if (message is ActiveMQTextMessage)
+            {
+                ActiveMQTextMessage msg = message as ActiveMQTextMessage;
+                Console.WriteLine("receive:" + msg.Text);
+               
+            }
+        }
+
+    }
+}
---
+++ TranVanHoangAnh_17045611/SendMessage/Send.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ.Commands;
+
+namespace SendMessage
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616");
+            IConnection con = factory.CreateConnection("admin", "admin");
+            con.Start();
+            ISession session = con.CreateSession(AcknowledgementMode.AutoAcknowledge);            
+            ActiveMQQueue destination = new ActiveMQQueue("HoangAnh");
+            IMessageProducer producer = session.CreateProducer(destination);
+            
+            for(int i=0; i<1000; i++)
+            {
+                IMessage msg = new ActiveMQTextMessage("Hello "+i);
+                producer.Send(msg);
+            }
+               
+            session.Close();
+            con.Close();
+        }
+    }
+}
